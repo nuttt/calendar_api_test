08f0086d6abffe6ac0252782ad285bb5ff1a4015
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2013-10-22 11:35:09 +0700

    first commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, '737888584320.apps.googleusercontent.com', 'y4GBlFgEm4GQaDLYqBcezp2X', {
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+    redirect_uri:'http://localhost/auth/google_oauth2/callback'
+  }
+  
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CalendarApiTest::Application.config.session_store :cookie_store, key: '_calendar_api_test_session'
